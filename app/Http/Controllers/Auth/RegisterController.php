8b0025dbff56f9c3dd27a875f82990f2744e3aad
sreerajtr <trsreeraj07@gmail.com>
2025-10-14 06:29:24 +0530

    registercontroller created

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -5,7 +5,6 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
-
 class RegisterController extends Controller
 {
     public function showRegistrationForm()
